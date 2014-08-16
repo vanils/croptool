ee94d771088e95a66fd250949be6b3b6c3645d7c
vanils <mail@vanils.com>
2014-08-16 21:25:29 +0300

    Initial commit

---
+++ build/jquery.croptool.js
@@ -0,0 +1,16 @@
+/*
+ *
+ * jQuery Croptool v1.0.0
+ * http://github.com/vanils/croptool
+ *
+ * Copyright 2014, Matti Mehtonen
+ * Licensed under the MIT license.
+ * http://github.com/vanils/croptool/blob/master/LICENSE
+ *
+ */
+
+(function (window, document, $) {
+
+"use strict";
+
+}(window, document, window.jQuery));
